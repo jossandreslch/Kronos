4a37dc424080539913dba14849fe200d10a5f776
Deimos <lozadacja@gmail.com>
2022-04-14 16:27:41 -0300

    Finalizamos el diseño responsive del navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,10 @@ function mobileMenu(){
     }
 }
 
+function closeMobileMenu(){
+    document.getElementById("nav-bar-mobile").style.display="none";
+}
+
 function activeOption(){
     var mobileMenu = document.getElementById("nav-bar-mobile");
 
@@ -18,3 +22,59 @@ function activeOption(){
         activeOp.style.color="#fff";
     }
 }
+
+/*
+
+FUNCIONES QUE TRATARON DE OCULTAR EL NAVBAR MOBILE AL HACER CLICK FUERA DEL MISMO, REVISAR DESPUÉS:
+
+function outToggle(){
+
+    document.addEventListener('click', function(event) {
+
+        var isClickInsideElement = document.getElementById("nav-bar-mobile").contains(event.target);
+
+        if ((document.getElementById("nav-bar-mobile").style.display="block") && (!isClickInsideElement)){
+            document.getElementById("nav-bar-mobile").style.display = 'none';
+        } 
+        
+        else if ((document.getElementById("nav-bar-mobile").style.display="none") && (!isClickInsideElement)){
+            document.getElementById("nav-bar-mobile").style.display = 'none';
+        }
+       
+        else{
+            document.getElementById("nav-bar-mobile").style.display = 'block';
+        }
+})
+}
+
+*/
+
+
+/*
+function toggleOutside(){
+
+    document.addEventListener('click', function(event) {
+        var isClickInsideElement = document.getElementById("iconMenu").contains(event.target);
+        if (!isClickInsideElement) {
+            document.getElementById("nav-bar-mobile").style.display = 'none';
+        } else{
+            document.getElementById("nav-bar-mobile").style.display = 'block';
+        }
+    })
+
+}
+
+    /*
+    var mobileMenu = document.getElementById("nav-bar-mobile");
+
+    if (mobileMenu.style.display=='block'){
+        document.addEventListener('click', function(event) {
+            var isClickInsideElement = mobileMenu.contains(event.target);
+            if (!isClickInsideElement) {
+                mobileMenu.style.display = 'none';
+            } else{
+                mobileMenu.style.display = 'block';
+            }
+        })
+    }
+    */
