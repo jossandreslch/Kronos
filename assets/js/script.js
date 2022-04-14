b5ac6830e7ec5ca6663aa04117935fc5db081394
Deimos <lozadacja@gmail.com>
2022-04-13 23:15:41 -0300

    Guardamos el progreso del diseño responsive para el navbar mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-alert("This code is succesfully running! 1")
+function showMenu(){
+    var mobileMenu = document.getElementById("nav-bar-mobile");
+    mobileMenu.style.display="block";
+}
+
+function activeOption(){
+
+    alert("This script is working");
+
+    var activeOp = document.getElementById("iconMenu");
+    activeOp.style.color="#ff5328";
+}
